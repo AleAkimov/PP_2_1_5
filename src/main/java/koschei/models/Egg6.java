0ae8d91598355ea944ba36259e445262539e00c3
AleAkimov <akimov897@gmail.com>
2024-07-18 20:18:30 +0300

    fixed errors

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,17 +1,21 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Egg6 {
-    private final Needle7 needle7;
-
-    public Egg6(Needle7 needle7) {
-        this.needle7 = needle7;
-    }
+    private  final Needle7 needle;
+    @Autowired
+    public Egg6(Needle7 needle) {this.needle = needle;}
+//    private final Needle7 needle7;
+//
+//    public Egg6(Needle7 needle7) {
+//        this.needle7 = needle7;
+//    }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + needle7.toString();
+        return ", в яйце иголка " + needle.toString();
     }
 }
--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -5,15 +5,20 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
-    private final Duck5 duck5;
-
+//    private final Duck5 duck5;
+//
+//    @Autowired
+//    public Rabbit4(Duck5 duck5) {
+//        this.duck5 = duck5;
+//    }
     @Autowired
-    public Rabbit4(Duck5 duck5) {
-        this.duck5 = duck5;
+    private Duck5 duck;
+
+    public Rabbit4(Duck5 duck) {this.duck = duck;
     }
 
     @Override
     public String toString() {
-        return ", в зайце утка " + duck5.toString();
+        return ", в зайце утка " + duck.toString();
     }
 }
